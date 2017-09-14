e494ac6975c253ed32b7238067e1516385e170c5
Hai Nguyen <phihai91@Hai.local>
2017-09-14 17:56:48 +0700

    install Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:
+  {
+    apiKey: "AIzaSyBNb5VfoD0tseieB__3t3Pu7j8mztAfw-w",
+    authDomain: "liminmanage.firebaseapp.com",
+    databaseURL: "https://liminmanage.firebaseio.com",
+    projectId: "liminmanage",
+    storageBucket: "",
+    messagingSenderId: "626196420890"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:
+  {
+    apiKey: "AIzaSyBNb5VfoD0tseieB__3t3Pu7j8mztAfw-w",
+    authDomain: "liminmanage.firebaseapp.com",
+    databaseURL: "https://liminmanage.firebaseio.com",
+    projectId: "liminmanage",
+    storageBucket: "",
+    messagingSenderId: "626196420890"
+  }
 };
