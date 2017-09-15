cf99bcabb0c9d6c38734a5558a9531d17a618c56
Hai Nguyen <phihai91@Hai.local>
2017-09-15 15:41:56 +0700

    logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
   isCollapsed;
+  user: firebase.User;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afAuth : AngularFireAuth) { 
+    afAuth.authState.subscribe(user => this.user = user);
+    afAuth.authState.subscribe(x=>console.log(x)); //log user information
   }
 
+  logout(){
+    this.afAuth.auth.signOut();
+  }
 }
