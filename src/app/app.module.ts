043244b9f84cc6feb2f306b1844d7948fd14d50d
Hai Nguyen <phihai91@Hai.local>
2017-09-15 09:36:46 +0700

    Router Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,42 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { StudentsComponent } from './students/students.component';
+import { CoursesComponent } from './courses/courses.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { BillsComponent } from './bills/bills.component';
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    StudentsComponent,
+    CoursesComponent,
+    TeachersComponent,
+    BillsComponent,
+    AdminUsersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'students', component: StudentsComponent },
+      { path: 'courses', component: CoursesComponent},
+      { path: 'teachers', component: TeachersComponent },
+      { path: 'bills', component: BillsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   firebase:
   {
-    apiKey: "AIzaSyBNb5VfoD0tseieB__3t3Pu7j8mztAfw-w",
-    authDomain: "liminmanage.firebaseapp.com",
-    databaseURL: "https://liminmanage.firebaseio.com",
-    projectId: "liminmanage",
-    storageBucket: "",
-    messagingSenderId: "626196420890"
+    apiKey: "AIzaSyBWN5cwmbubBbBY_bVzT0VuHIf7GGURhw4",
+    authDomain: "liminmanage-79973.firebaseapp.com",
+    databaseURL: "https://liminmanage-79973.firebaseio.com",
+    projectId: "liminmanage-79973",
+    storageBucket: "liminmanage-79973.appspot.com",
+    messagingSenderId: "388759218306"
   }
 };
