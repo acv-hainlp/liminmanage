3c6f60dd2256c8f6bc39ccda137378bee81554f6
Hai Nguyen <phihai91@Hai.local>
2017-09-18 12:30:30 +0700

    redirect after new courses

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -1,13 +1,15 @@
+import { Router } from '@angular/router';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CourseService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private router: Router) { }
 
   create(course) {
     this.db.list('/courses').push(course);
+    this.router.navigate(['/courses']);
   }
 
 }
