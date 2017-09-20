9114dd01c8729e2cdc5202f7417afcedea14ab4e
Hai Nguyen <phihai91@Hai.local>
2017-09-20 18:20:11 +0700

    class add student

--- src/app/admin/components/class-form/class-form.component.ts
+++ src/app/admin/components/class-form/class-form.component.ts
@@ -1,3 +1,4 @@
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { StudentService } from '../../../shared/services/student.service';
 import { CourseService } from './../../../shared/services/course.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
@@ -8,16 +9,37 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./class-form.component.css']
 })
 export class ClassFormComponent implements OnInit {
+  studentsId = [];
+  studentsName = [];
+
+  students$;
   courses$;
   teachers$;
 
-  constructor(private courseService: CourseService, private studentService: StudentService) {
+  constructor(private courseService: CourseService, private studentService: StudentService ) {
     this.courses$ = courseService.getAll();
     this.teachers$ = studentService.getTeacher();
+    this.students$ = studentService.getAll();
    }
+   
+  save() {
 
+  }
 
-   ngOnInit() {
+  addStudent(studentId, studentName){
+    this.studentsId.push(studentId);
+    this.studentsName.push(studentName);
+    // JSON.stringify(this.students);
+    // console.log(this.studentsId);
+    // console.log(this.studentsName);
+  }
+
+  removeStudent(index)
+  {
+    this.studentsId.splice(index, 1);
+    this.studentsName.splice(index, 1);
+  }
+  ngOnInit() {
   }
 
 }
